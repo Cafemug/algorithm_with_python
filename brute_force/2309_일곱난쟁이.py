41efe675ac2dc4f54a390d9c281a9ce7a8ec6dc4
coco.ball <coco.ball@kakaoenterprise.com>
2021-01-10 23:13:22 +0900

    2309 일곱난쟁이

---
+++ brute_force/2309_일곱난쟁이.py
@@ -0,0 +1,26 @@
+import sys
+
+def print_result(arr, i, j):
+    for x in range(9):
+        if x != i and x != j: 
+            print(arr[x])
+    
+def filter_dwarf(dwarf_list):
+    total_sum = sum(dwarf_list)
+    for i in range(9):
+        for j in range(9):
+            if i != j:
+                if total_sum - dwarf_list[i] - dwarf_list[j] == 100:
+                    print_result(dwarf_list, i, j)
+                    return
+
+dwarf_list = []
+for _ in range(9):
+    dwarf_list.append(int(sys.stdin.readline()))
+
+dwarf_list.sort()
+filter_dwarf(dwarf_list)
+
+
+                
+            
