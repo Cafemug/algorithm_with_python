af5c222e8cb1d796bab177b9eb2bfcf9efcf0756
cafemug <hyanghope@gmail.com>
2021-01-25 23:47:00 +0900

    fix: main함수 추가

--- brute_force/2309_일곱난쟁이.py
+++ brute_force/2309_일곱난쟁이.py
@@ -13,14 +13,14 @@ def filter_dwarf(dwarf_list):
                 if total_sum - dwarf_list[i] - dwarf_list[j] == 100:
                     print_result(dwarf_list, i, j)
                     return
+def main():
+    dwarf_list = []
+    for _ in range(9):
+        dwarf_list.append(int(sys.stdin.readline()))
 
-dwarf_list = []
-for _ in range(9):
-    dwarf_list.append(int(sys.stdin.readline()))
+    dwarf_list.sort()
+    filter_dwarf(dwarf_list)
 
-dwarf_list.sort()
-filter_dwarf(dwarf_list)
-
-
+main()
                 
             
